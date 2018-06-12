f8fbda82720488ff5ca4eb023e30025de1a8a133
GalArt <galart@t-sk.ru>
2018-06-12 22:26:57 +0700

    add try catch

--- test.groovy
+++ test.groovy
@@ -1,5 +1,13 @@
-node('master'){
-    println "Hello"
-    error '11'
-
+try{
+    node('master'){
+        println "Hello"
+        error '11'
+        println 'her'
+    }
+}
+catch(err){
+    println err
+}
+finally {
+    println 'finish '+currentBuild.result
 }
