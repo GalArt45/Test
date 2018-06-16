cc08fd16abf9678553c364fec79aaaf36461e925
GalArt <Galart@t-sk.ru>
2018-06-16 13:57:11 +0300

    test

--- test.groovy
+++ test.groovy
@@ -1,8 +1,27 @@
 try{
-    node('master'){
-        println "Hello"
-        error '11'
-        println 'her'
+    node("centos") {
+        cleanWs()
+        GitPath="/*"
+        GitURL="https://github.com/GalArt45/Test"
+        GitBranch="*/master"
+        checkout([$class: 'GitSCM', branches: [[name: GitBranch]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'GalArt45', url: GitURL]]])
+        sh "ls -la"
+        withCredentials([usernamePassword(credentialsId: 'vault', passwordVariable: 'VAULT_PASS', usernameVariable: 'user')]) {
+            sh 'chmod +x print_vault.sh'
+            echo "LS=${VAULT_PASS}"
+            println "Замена конфигов."
+            ansiblePlaybook (
+                colorized: true,
+                playbook: 'test.yml',
+                inventory: 'hosts/hosts',
+                extras: '--vault-password-file=print_vault.sh',
+                tags: 'build_app',
+                extraVars: [
+                    SP_VERSION: currentBuild.number
+                ]
+            )
+        }
+        cleanWs()
     }
 }
 catch(err){
